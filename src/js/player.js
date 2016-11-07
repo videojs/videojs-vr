2b9a865621fe7801ac8d2ddeca656c4fff9b5d00
James Broberg <james.broberg@metacdn.com>
2016-11-07 14:04:38 -0800

    Add initial dash support

--- src/js/player.js
+++ src/js/player.js
@@ -1,4 +1,6 @@
 var videojs = global.videojs = require('video.js');
+require('dashjs');
+require('videojs-contrib-dash');
 global.THREE = require('three');
 require('three/examples/js/controls/VRControls.js');
 require('three/examples/js/effects/VREffect.js');
