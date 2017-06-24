83b2365272a02339a699db897117b26bdf23462f
Mitchell Trent <Mitch@tofi.com.au>
2017-06-24 17:18:00 +1000

    added in vr button to the control bar

--- src/js/videojs.vr.js
+++ src/js/videojs.vr.js
@@ -340,7 +340,7 @@
             // Previous timestamps for gamepad updates
             var prevTimestamps = [];
 
-            var manager = new WebVRManager(renderer, effect, {hideButton: false});
+            var manager = new WebVRManager(renderer, effect, {hideButton: true});
 
             renderedCanvas = renderer.domElement;
             renderedCanvas.style.width = "inherit";
@@ -516,129 +516,44 @@
                   renderer: renderer
               }
             };
+
         }
 
         player.on('loadstart', function () {
            initScene();
         });
 
-     /**
-      * Add the menu options
-      */
-     function initMenu() {
-         var ProjectionSelector = vjs.extend(vjs.getComponent('MenuButton'), {
-             constructor : function(player, options) {
-                 player.availableProjections = options.availableProjections || [];
-                 vjs.getComponent('MenuButton').call(this, player, options);
-                 var items = this.el().firstChild
-                 var wrapper = vjs.createEl("div", {
-                     className : 'vjs-control-content'
-                 })
-                 this.el().replaceChild(wrapper, items)
-                 wrapper.appendChild(items)
-             }
-         });
+    function isMobile() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    }
 
-         vjs.registerComponent('ProjectionSelector', ProjectionSelector);
-
-         var ProjectionSelection = vjs.extend(vjs.getComponent('Button'), {
-             constructor : function(player, options) {
-               this.availableProjections = options.availableProjections || [];
+     function addVrControls(cb) {
+         var Button = videojs.getComponent('Button');
+         console.log(Button);
+         var MyButton = videojs.extend(Button, {
+             constructor: function() {
+                 Button.apply(this, arguments);
+                 console.log(this);
+                 /* initialize your button */
              },
-             className : 'vjs-res-button vjs-menu-button vjs-control',
-             role    : 'button',
-             'aria-live' : 'polite', // let the screen reader user know that the text of the button may change
-             tabIndex  : 0
-         });
-
-         vjs.registerComponent('ProjectionSelection', ProjectionSelection);
-
-         //Top Item - not selectable
-         var ProjectionTitleMenuItem = vjs.extend(vjs.getComponent('MenuItem'), {
-             constructor : function(player, options) {
-                 vjs.getComponent('MenuItem').call(this, player, options);
-                 this.off('click'); //no click handler
+             handleClick: function() {
+                var event = new Event('vrdisplayactivate');
+                window.dispatchEvent(event);
+                 /* do something on click */
+             },
+             buildCSSClass: function() {
+                 return Button.prototype.buildCSSClass.call(this) + 'vjs-button-vr';
              }
          });
-
-         vjs.registerComponent('ProjectionTitleMenuItem', ProjectionTitleMenuItem);
-
-         //Menu Item
-         var ProjectionMenuItem = vjs.extend(vjs.getComponent('MenuItem'), {
-             constructor : function(player, options){
-                 options.label = options.res;
-                 options.selected = (options.res.toString() === player.getCurrentRes().toString());
-                 vjs.getComponent('MenuItem').call(this, player, options);
-                 this.resolution = options.res;
-                 this.on('click', this.onClick);
-                 player.on('changeProjection', vjs.bind(this, function() {
-                     if (this.resolution === player.getCurrentRes()) {
-                         this.selected(true);
-                     } else {
-                         this.selected(false);
-                     }
-                 }));
-             }
-         });
-
-         // Handle clicks on the menu items
-         ProjectionMenuItem.prototype.onClick = function() {
-             var player = this.player(),
-             button_nodes = player.controlBar.projectionSelection.el().firstChild.children,
-             button_node_count = button_nodes.length;
-
-             // Save the newly selected resolution in our player options property
-             player.current_proj = this.resolution
-             changeProjection(this.resolution);
-             player.trigger('changeProjection');
-
-             // Update the button text
-             while ( button_node_count > 0 ) {
-                 button_node_count--;
-                 if ( 'vjs-current-res' === button_nodes[button_node_count].className ) {
-                     button_nodes[button_node_count].innerHTML = this.resolution;
-                     break;
-                 }
-             }
-         };
-
-         vjs.registerComponent('ProjectionMenuItem', ProjectionMenuItem);
-
-         // Create a menu item for each available projection
-         vjs.getComponent('ProjectionSelector').prototype.createItems = function() {
-             var player = this.player(),
-             items = [];
-
-             // Add the menu title item
-             items.push( new vjs.getComponent('ProjectionTitleMenuItem')( player, {
-                 el : vjs.createEl( 'li', {
-                     className : 'vjs-menu-title vjs-res-menu-title',
-                     innerHTML : 'Projections'
-                 })
-             }));
-
-             // Add an item for each available resolution
-             player.availableProjections.forEach(function (proj) {
-                 items.push( new vjs.getComponent('ProjectionMenuItem')( player, {
-                     res : proj
-                 }));
-             });
-
-             return items;
-         };
-
+         videojs.registerComponent('MyButton', MyButton);
+         cb.addChild('myButton', {});
      }
-     initMenu();
 
-     function addMenu(cb) {
-         player.getCurrentRes = function() {
-             return player.current_proj || '';
-         };
-
-         // Add the button to the control bar object and the DOM
-         cb.projectionSelection = cb.addChild( 'ProjectionSelection' );
+     if(isMobile()){
+        addVrControls(player.controlBar)
      }
-     addMenu(player.controlBar);
 
      return {
          changeProjection: changeProjection
