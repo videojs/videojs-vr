cab275526678bec09b14e5b815dc0dc7542f8037
brandonocasey <brandonocasey@gmail.com>
2017-08-01 17:45:40 -0400

    code review comments

--- src/plugin.js
+++ src/plugin.js
@@ -1,32 +1,10 @@
 import window from 'global/window';
-
-// this is used to configure webvr-polyfill
-// see https://github.com/googlevr/webvr-polyfill#configuration
-const WebVRConfig = {
-  // TODO:
-  // implement controls so that thy only work when the player is in focus.
-  // This will require MOUSE_KEYBOARD_CONTROLS_DISABLED, and TOUCH_PANNER_DISABLED to be set to true
-  // and various points in the code will have to be changed. As a lot of the code
-  // expects some sort of controls to be set up, and when no controls are set up
-  // it throws an error about no vr displays being found. VrDisplays is actually referring to controls.
-
-  // Flag to disable touch panner. In case you have your own touch controls.
-  // Default: true.
-  // TOUCH_PANNER_DISABLED: true,
-  // To disable keyboard and mouse controls, if you want to use your own
-  // implementation.
-  // Default: false.
-  // MOUSE_KEYBOARD_CONTROLS_DISABLED: true,
-};
-
-window.WebVRConfig = WebVRConfig;
+/* THIS CONFIGURES webvr-polyfill don't change the order */
+import './webvr-config.js';
 import 'webvr-polyfill/src/main';
-
 import videojs from 'video.js';
 import {version as VERSION} from '../package.json';
-
 import * as THREE from 'three';
-
 // previously we used
 // * three/examples/js/controls/VRControls.js
 // * three/examples/js/effects/VREffects.js
@@ -34,7 +12,6 @@ import * as THREE from 'three';
 // so the code has been copied locally to allow exporting
 import VRControls from './VRControls.js';
 import VREffect from './VREffect.js';
-
 import WebVRManager from 'webvr-boilerplate';
 
 window.WebVRManager = WebVRManager;
