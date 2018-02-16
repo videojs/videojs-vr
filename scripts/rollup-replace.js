09b15592c7a58f733ea649512ce24b5d45d1943b
brandonocasey <brandonocasey@gmail.com>
2018-02-16 12:16:37 -0500

    fix: rework three example build, to fix webpack

--- scripts/rollup-replace.js
+++ scripts/rollup-replace.js
@@ -1,18 +1,33 @@
-import replace from 'rollup-plugin-replace';
+import replace from 'rollup-plugin-re';
+
+// three modules to find-replace in
+const modules = [
+  'VRControls',
+  'VREffect',
+  'OrbitControls'
+];
 
 export default function(options) {
-
   return replace(Object.assign({
-    'include': ['node_modules/three/examples/js/**'],
-    'delimiters': ['', ''],
-    'THREE.VRControls =': "var THREE = require('three');var VRControls;\nmodule.exports = VRControls =",
-    'THREE.VRControls': 'VRControls',
+    include: ['node_modules/three/examples/js/**'],
+    patterns: [
+      {transform(code, id) {
+        modules.forEach((m) => {
+          if (!(new RegExp(m)).test(id)) {
+            return;
+          }
 
-    'THREE.VREffect =': "var THREE = require('three');var VREffect;\nmodule.exports = VREffect =",
-    'THREE.VREffect': 'VREffect',
+          // trun the global modifiction into an import and a local variable definition
+          code = code.replace(`THREE.${m} =`, `import * as THREE from 'three';\nconst ${m} =`);
 
-    'THREE.OrbitControls =': "var THREE = require('three');var OrbitControls;\nmodule.exports = OrbitControls =",
-    'THREE.OrbitControls': 'OrbitControls'
-  }, options || {}));
+          // change references from the global modification to the local variable
+          code = code.replace(new RegExp(`THREE.${m}`, 'g'), m);
+
+          // export that local variable as default from this module
+          code += `\nexport default ${m};`;
+        });
+        return code;
+      }}
+    ]}, options || {}));
 }
 
