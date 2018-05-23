0cde01610830d634d7a1b91accb75ab4824f165f
Brandon Casey <BrandonOCasey@users.noreply.github.com>
2018-05-23 15:59:10 -0400

    fix: re-implement touch pan controls (#89)

--- scripts/rollup-replace.js
+++ scripts/rollup-replace.js
@@ -4,8 +4,13 @@ import replace from 'rollup-plugin-re';
 const modules = [
   'VRControls',
   'VREffect',
-  'OrbitControls'
+  'OrbitControls',
+  'DeviceOrientationControls'
 ];
+
+const globalReplace = function(str, pattern, replacement) {
+  return str.replace(new RegExp(pattern, 'g'), replacement);
+};
 
 export default function(options) {
   return replace(Object.assign({
@@ -21,10 +26,20 @@ export default function(options) {
           code = code.replace(`THREE.${m} =`, `import * as THREE from 'three';\nvar ${m} =`);
 
           // change references from the global modification to the local variable
-          code = code.replace(new RegExp(`THREE.${m}`, 'g'), m);
+          code = globalReplace(code, `THREE.${m}`, m);
 
           // export that local variable as default from this module
           code += `\nexport default ${m};`;
+
+          // expose private functions so that users can manually use controls
+          // and we can add orientation controls
+          if (m === 'OrbitControls') {
+            code = globalReplace(code, 'function rotateLeft\\(', 'rotateLeft = function(');
+            code = globalReplace(code, 'function rotateUp\\(', 'rotateUp = function(');
+
+            code = globalReplace(code, 'rotateLeft', 'scope.rotateLeft');
+            code = globalReplace(code, 'rotateUp', 'scope.rotateUp');
+          }
         });
         return code;
       }}
