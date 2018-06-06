a7b6be12303345625ac68fe4ad901310dffcde30
brandonocasey <brandonocasey@gmail.com>
2018-06-06 11:20:45 -0400

    better user activity blocking

--- src/canvas-player-controls.js
+++ src/canvas-player-controls.js
@@ -8,8 +8,6 @@ import videojs from 'video.js';
  *    `touchmove` or `mousemove` toggles play/pause on the player
  * 2. Only moving on/clicking the control bar or toggling play/pause should
  *    show the control bar. Moving around the scene in the canvas should not.
- *
- *
  */
 class CanvasPlayerControls extends videojs.EventTarget {
   constructor(player, canvas) {
@@ -21,7 +19,23 @@ class CanvasPlayerControls extends videojs.EventTarget {
     this.onMoveEnd = videojs.bind(this, this.onMoveEnd);
     this.onMoveStart = videojs.bind(this, this.onMoveStart);
     this.onMove = videojs.bind(this, this.onMove);
-    this.controlBarMove = videojs.bind(this, this.controlBarMove);
+    this.onControlBarMove = videojs.bind(this, this.onControlBarMove);
+
+    this.player.controlBar.on([
+      'mousedown',
+      'mousemove',
+      'mouseup',
+      'touchstart',
+      'touchmove',
+      'touchend'
+    ], this.onControlBarMove);
+
+    // we have to override these here because
+    // video.js listens for user activity on the video element
+    // and makes the user active when the mouse moves.
+    // We don't want that for 3d videos
+    this.oldReportUserActivity = this.player.reportUserActivity;
+    this.player.reportUserActivity = () => {};
 
     // canvas movements
     this.canvas.addEventListener('mousedown', this.onMoveStart);
@@ -31,34 +45,7 @@ class CanvasPlayerControls extends videojs.EventTarget {
     this.canvas.addEventListener('mouseup', this.onMoveEnd);
     this.canvas.addEventListener('touchend', this.onMoveEnd);
 
-    this.player.controlBar.on([
-      'mousedown',
-      'mousemove',
-      'mouseup',
-      'touchstart',
-      'touchmove',
-      'touchend'
-    ], this.controlBarMove);
-
     this.shouldTogglePlay = false;
-
-    this.oldUserActive_ = this.player.userActive;
-    this.player.userActive = (v) => {
-      if (typeof v === 'undefined') {
-        return this.oldUserActive_.call(this.player);
-      }
-
-      if (v !== this.userActive_) {
-        return;
-      }
-
-      return this.oldUserActive_.call(this.player, v);
-    };
-  }
-
-  userActive(v) {
-    this.userActive_ = v;
-    this.player.userActive(v);
   }
 
   togglePlay() {
@@ -70,41 +57,38 @@ class CanvasPlayerControls extends videojs.EventTarget {
   }
 
   onMoveStart(e) {
-    this.userActive(false);
 
-    // if the player does not have a controlbar do no toggle play
-    if (!this.player.controls()) {
-      this.shouldTogglePlay = false;
-      return;
-    }
-
-    // if the move start was a mouse click but not left click
-    // do not toggle play
-    if (e.type === 'mousedown' && !videojs.dom.isSingleLeftClick(e)) {
+    // if the player does not have a controlbar or
+    // the move was a mouse click but not left click do not
+    // toggle play.
+    if (!this.player.controls() || (e.type === 'mousedown' && !videojs.dom.isSingleLeftClick(e))) {
       this.shouldTogglePlay = false;
       return;
     }
 
     this.shouldTogglePlay = true;
+    this.touchMoveCount_ = 0;
   }
 
   onMoveEnd(e) {
     if (!this.shouldTogglePlay) {
-      this.userActive_ = false;
-      this.userActive(false);
       return;
     }
-    this.userActive(true);
     this.togglePlay();
   }
 
   onMove(e) {
+    // its hard to tap without a touchmove, if there have been less
+    // than one, we should still toggle play
+    if (e.type === 'touchmove' && this.touchMoveCount_ < 1) {
+      this.touchMoveCount_++;
+      return;
+    }
     this.shouldTogglePlay = false;
-    this.userActive(false);
   }
 
-  controlBarMove(e) {
-    this.userActive(true);
+  onControlBarMove(e) {
+    this.player.userActive(true);
   }
 
   dispose() {
@@ -122,9 +106,9 @@ class CanvasPlayerControls extends videojs.EventTarget {
       'touchstart',
       'touchmove',
       'touchend'
-    ], this.controlBarMove);
+    ], this.onControlBarMove);
 
-    this.player.userActive = this.oldUserActive_;
+    this.player.reportUserActivity = this.oldReportUserActivity;
   }
 }
 
