77a1d079c11d2e658b0ce130c1d9af0deb9a4ce3
brandonocasey <brandonocasey@gmail.com>
2018-08-22 12:35:09 -0400

    chore: generator v7

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -1,77 +1,9 @@
-/* eslint-disable no-console */
-const postcss = require('postcss');
-const path = require('path');
-const pkg = require('../package.json');
-const banner = `@name ${pkg.name} @version ${pkg.version} @license ${pkg.license}`;
-const getNow = () => Date.now();
-
-let startTime = getNow();
-
-/**
- * by default there is no way to print that file was written
- * this does that.
- */
-const printOutput = postcss.plugin('postcss-print-output', function(opts) {
-  opts = opts || {};
-
-  return function(root, results) {
-    const relativeFrom = path.relative(process.cwd(), results.opts.from);
-    const relativeTo = path.relative(process.cwd(), results.opts.to);
-
-    console.log(`${relativeFrom} -> ${relativeTo} in ${getNow() - startTime}ms`);
-  };
-});
-
-/**
- * A function to set the startTime of postcss so that
- * we can print the time taken in the output.
- */
-const setTime = postcss.plugin('postcss-set-time', function(opts) {
-  return function(root, results) {
-    startTime = getNow();
-  };
-});
+const generate = require('videojs-generate-postcss-config');
 
 module.exports = function(context) {
-  return {
-    plugins: [
-      // set the startTime so that we can print the end time
-      setTime(),
+  const result = generate({}, context);
 
-      // inlines local file imports
-      require('postcss-import')(),
+  // do custom stuff here
 
-      // allows you to use newer css features, by converting
-      // them into something browsers can support now.
-      // see https://preset-env.cssdb.org/features
-      // by default we use stage 3+
-      require('postcss-preset-env')({
-        browsers: pkg.browserslist,
-        stage: false,
-        features: {
-          // turn `var(xyz)` in the actual value
-          'custom-properties': {preserve: false, warnings: true},
-
-          // flatten nested rules
-          'nesting-rules': true
-        }
-      }),
-
-      // adds a banner to the top of the file
-      require('postcss-banner')({important: true, inline: true, banner}),
-
-      // add/remove vendor prefixes based on browser list
-      require('autoprefixer')(pkg.browserslist),
-
-      // minify
-      require('cssnano')({
-        safe: true,
-        preset: ['default', {
-          autoprefixer: pkg.browserslist
-        }]
-      }),
-
-      printOutput()
-    ]
-  };
+  return result;
 };
--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,188 +1,25 @@
-/**
- * Rollup configuration for packaging the plugin in various formats.
- */
-const babel = require('rollup-plugin-babel');
-const commonjs = require('rollup-plugin-commonjs');
-const json = require('rollup-plugin-json');
-const multiEntry = require('rollup-plugin-multi-entry');
-const resolve = require('rollup-plugin-node-resolve');
-const {uglify} = require('rollup-plugin-uglify');
-const {minify} = require('uglify-es');
-const pkg = require('../package.json');
+const generate = require('videojs-generate-rollup-config');
 const replace = require('./rollup-replace');
 
-/* to prevent going into a screen during rollup */
-process.stderr.isTTY = false;
-
-let isWatch = false;
-
-process.argv.forEach((a) => {
-  if ((/-w|--watch/).test(a)) {
-    isWatch = true;
-  }
-});
-
-/* configuration for plugins */
-const primedPlugins = {
-  babel: babel({
-    babelrc: false,
-    exclude: 'node_modules/**',
-    presets: [
-      ['env', {loose: true, modules: false, targets: {browsers: pkg.browserslist}}]
-    ],
-    plugins: [
-      'external-helpers',
-      'transform-object-assign'
-    ]
-  }),
-  commonjs: commonjs({sourceMap: false}),
-  json: json(),
-  multiEntry: multiEntry({exports: false}),
-  resolve: resolve({browser: true, main: true, jsnext: true}),
-  uglify: uglify({output: {comments: 'some'}}, minify),
-  replace
-};
-
-/* General Globals */
-const moduleName = 'videojsVr';
-const pluginName = 'videojs-vr';
-
-// globals, aka replace require calls with this
-const globals = {
-  umd: {
-    'video.js': 'videojs',
-    'global': 'window',
-    'global/window': 'window',
-    'global/document': 'document'
+// see https://github.com/videojs/videojs-generate-rollup-config
+// for options
+const options = {
+  primedPlugins(defaults) {
+    return Object.assign(defaults, {replace});
   },
-  module: {
-    'video.js': 'videojs'
-  },
-  test: {
-    'qunit': 'QUnit',
-    'qunitjs': 'QUnit',
-    'sinon': 'sinon',
-    'video.js': 'videojs'
+  plugins(defaults) {
+
+    // add replace just after json for each build
+    Object.keys(defaults).forEach((type) => {
+      defaults[type].splice(defaults[type].indexOf('json') + 1, '0', 'replace');
+    });
+
+    return defaults;
   }
 };
+const config = generate(options);
 
-// externals, aka don't bundle these and if not
-// listed as a global don't require them either
-const externals = {
-  umd: Object.keys(globals.umd).concat([
+// Add additonal builds/customization here!
 
-  ]),
-  module: Object.keys(globals.module).concat([
-    'global',
-    'global/document',
-    'global/window'
-  ]),
-  test: Object.keys(globals.test).concat([
-
-  ])
-};
-
-/* plugins that should be used in each bundle with caveats as comments */
-const plugins = {
-  // note uglify will be added before babel for minified bundle
-  umd: [
-    primedPlugins.resolve,
-    primedPlugins.json,
-    primedPlugins.replace,
-    primedPlugins.commonjs,
-    primedPlugins.babel
-  ],
-
-  module: [
-    primedPlugins.resolve,
-    primedPlugins.json,
-    primedPlugins.replace,
-    primedPlugins.commonjs,
-    primedPlugins.babel
-  ],
-
-  test: [
-    primedPlugins.multiEntry,
-    primedPlugins.resolve,
-    primedPlugins.json,
-    primedPlugins.replace,
-    primedPlugins.commonjs,
-    primedPlugins.babel
-  ]
-};
-
-/* make a build with the specifed settings */
-const makeBuild = (name, settings) => {
-  const b = Object.assign({}, {
-    plugins: plugins[name],
-    external: externals[name],
-    input: 'src/plugin.js'
-  }, settings);
-
-  const fixOutput = (o) => {
-    if (!o.banner) {
-      o.banner = `/*! @name ${pkg.name} @version ${pkg.version} @license ${pkg.license} */`;
-    }
-    if (!o.globals) {
-      o.globals = globals[name];
-    }
-
-    return o;
-  };
-
-  if (!Array.isArray(b.output)) {
-    b.output = fixOutput(b.output);
-  } else {
-    b.output = b.output.map(fixOutput);
-  }
-
-  return b;
-};
-
-/* all rollup builds by name. note only object values will be used */
-const builds = {
-  umd: makeBuild('umd', {
-    output: [{
-      name: moduleName,
-      file: `dist/${pluginName}.js`,
-      format: 'umd'
-    }]
-  }),
-  cjs: makeBuild('module', {
-    output: [{
-      file: `dist/${pluginName}.cjs.js`,
-      format: 'cjs'
-    }]
-  }),
-  es: makeBuild('module', {
-    output: [{
-      file: `dist/${pluginName}.es.js`,
-      format: 'es'
-    }]
-  }),
-  test: makeBuild('test', {
-    input: 'test/**/*.test.js',
-    output: [{
-      name: `${moduleName}Tests`,
-      file: 'test/dist/bundle.js',
-      format: 'iife'
-    }]
-  })
-};
-
-if (!isWatch) {
-  builds.minUmd = makeBuild('umd', {
-    output: [{
-      name: moduleName,
-      file: `dist/${pluginName}.min.js`,
-      format: 'umd'
-    }],
-    // we need to minify before babel
-    plugins: plugins.umd
-      .filter((p) => p !== primedPlugins.babel)
-      .concat([primedPlugins.uglify, primedPlugins.babel])
-  });
-
-}
-
-export default Object.values(builds);
+// export the builds to rollup
+export default Object.values(config.builds);
