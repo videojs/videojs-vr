f6974f8c53e5eead2fd5530107a7cbf9f7ddb239
Kevin Staunton-Lambert <kevleyski@gmail.com>
2020-08-20 11:50:02 +1000

    Fixes #226 VR180 monoscopic is cropped
    (part revert - review)

--- src/plugin.js
+++ src/plugin.js
@@ -593,7 +593,7 @@ void main() {
     // Store vector representing the direction in which the camera is looking, in world space.
     this.cameraVector = new THREE.Vector3();
 
-    if (this.currentProjection_ === '360_LR' || this.currentProjection_ === '360_TB' || this.currentProjection_ === '180' || this.currentProjection_ === 'EAC_LR') {
+    if (this.currentProjection_ === '360_LR' || this.currentProjection_ === '360_TB' || this.currentProjection_ === '180' || this.currentProjection_ === '180_LR' || this.currentProjection_ === '180_MONO' || this.currentProjection_ === 'EAC_LR') {
       // Render left eye when not in VR mode
       this.camera.layers.enable(1);
     }
