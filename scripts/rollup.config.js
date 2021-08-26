e8645455e254ae357fecc7236fc53c62e0741669
Gary Katsevman <git@gkatsev.com>
2021-08-26 16:27:20 -0400

    simplify rollup script

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,25 +1,8 @@
 const generate = require('videojs-generate-rollup-config');
-const replace = require('./rollup-replace');
-const exampleResolver = require('./rollup-example-resolver');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
-const options = {
-  primedPlugins(defaults) {
-    return Object.assign(defaults, {exampleResolver,replace});
-  },
-  plugins(defaults) {
-    defaults.module.unshift('exampleResolver')
-
-    // add replace just after json for each build
-    Object.keys(defaults).forEach((type) => {
-      defaults[type].splice(defaults[type].indexOf('json') + 1, '0', 'replace');
-    });
-
-    return defaults;
-  }
-};
-const config = generate(options);
+const config = generate({});
 
 // Add additional builds/customization here!
 
