dbc10739e223923620ada09fb9ee11eb3b492db3
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2022-12-07 09:05:29 +1100

    [KJSL] Swap webvr ro webxr polyfill packages

--- src/plugin.js
+++ src/plugin.js
@@ -10,6 +10,7 @@ import OrbitOrientationContols from './orbit-orientation-controls.js';
 import * as utils from './utils';
 import CanvasPlayerControls from './canvas-player-controls';
 import OmnitoneController from './omnitone-controller';
+import { VRButton } from '../vendor/three/VRButton.js';
 
 // import controls so they get regisetered with videojs
 import './cardboard-button';
@@ -701,7 +702,9 @@ void main() {
       this.log('is supported, getting vr displays');
 
       window.navigator.xr.isSessionSupported('immersive-vr').then((displays) => {
-        // ShowEnterVRButton();
+
+        // ShowEnterVRButton
+        document.body.appendChild(VRButton.createButton(this.renderer));
 
         if (displays.length > 0) {
           this.log('Displays found', displays);
