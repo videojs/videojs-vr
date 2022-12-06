cd308066dd7c40033ed1177c1278ea4c03c419d0
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2022-12-07 09:15:46 +1100

    [KJSL] Swap webvr ro webxr polyfill packages

--- src/plugin.js
+++ src/plugin.js
@@ -1,3 +1,6 @@
+require('babel-polyfill');
+
+import 'babel-polyfill';
 import {version as VERSION} from '../package.json';
 import window from 'global/window';
 import document from 'global/document';
